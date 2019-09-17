d5a2dec4a234403fce055560c6d88db22334ff82
bbeale <ben.beale@medhub.com>
2019-09-17 16:57:16 -0400

    Broke up the gnarly method into two smaller ones.... much much much more work to come!

--- alpaca_test.py
+++ alpaca_test.py
@@ -29,7 +29,10 @@ api = tradeapi.REST(
 
 
 def get_stuff_to_trade():
+    """ Loops through all securities with volume data, grabs the best ones for trading according to the indicators
 
+    :return vol_assets: list of dictionaries representing intraday, MACD, RSI, RoC, and stochastic oscillators
+    """
     account                 = api.get_account()
     print("Account #:       {}".format(account.account_number))
     print("Currency:        {}".format(account.currency))
@@ -68,46 +71,102 @@ def get_stuff_to_trade():
             continue
 
         else:
+
+            print("Symbol: ", symbol)
+            # barset = api.get_barset(symbol[0], "15Min", after="2019-09-12T07:00:00-04:00")
+            # asset = api.get_asset(symbol[0])
+            # symbol_bars = barset[symbol[0]]
+
+            alfavantage = TimeSeries(key=config["alpha_vantage"]["API_KEY"], output_format="pandas")
+            indicators = TechIndicators(key=config["alpha_vantage"]["API_KEY"], output_format="pandas")
+
+            tradedata = dict()
+            tradedata["symbol"] = symbol
+            # Get current ticker price
+            tradedata["close"] = closeprices[0]
+            # Get json object with the intraday data and another with  the call's metadata
+            intraday, i_meta = alfavantage.get_intraday(symbol, interval="1min")
+            tradedata["intraday"] = intraday['4. close']
+            # Get the MACD
+            macd, macd_meta = indicators.get_macd(symbol, interval="1min")
+            tradedata["macd"] = macd["MACD"]
+            # Get the RSI
+            rsi, rsi_meta = indicators.get_rsi(symbol, interval="1min", time_period=100)
+            tradedata["rsi"] = rsi["RSI"]
+            # Get the RoC
+            roc, roc_meta = indicators.get_roc(symbol, interval="1min", time_period=100)
+            tradedata["roc"] = roc["ROC"]
+            # Get stochastic oscillator
+            stoc, stoc_meta = indicators.get_stoch(symbol, interval="1min")
+            tradedata["stoc"] = stoc
+            # And finally, calculate the limit
+            limit_price = closeprices[0] - closeprices[0] * .1
+            tradedata["limit"] = limit_price
+
             if len(volume) > 0:
                 vmean = mean(volume)
-                vol_assets.append((symbol, vmean, closeprices))
+                tradedata["vmean"] = vmean
+                vol_assets.append(tradedata)
+                # vol_assets.append((symbol, vmean, closeprices))
 
         if len(vol_assets) > 3:
             break
+    return vol_assets
 
-    symbol = sorted(vol_assets, key=lambda ass: ass[1], reverse=True)[0]
+
+def find_best_security(asset_list):
+
+    symbol = sorted(asset_list, key=lambda ass: ass["vmean"], reverse=True)[0]
     print("Symbol: ", symbol)
+
+    # https://www.lazyfa.com/
+
+
+
+    # TODO: Figure out what else I need to do with the data before trading it
+
+    # Also, might want a better tech indicator lib that won't throttle me
+    # https://www.worldtradingdata.com/pricing
+    # https://daytradingz.com/trade-ideas/
+    # https://daytradingz.com/gap-and-go-strategy/
+    # https://daytradingz.com/low-float-stocks/
+    # https://iextrading.com/developer/
+
+
+
+
+
     # barset = api.get_barset(symbol[0], "15Min", after="2019-09-12T07:00:00-04:00")
     # asset = api.get_asset(symbol[0])
     # symbol_bars = barset[symbol[0]]
 
-    alfavantage = TimeSeries(key=config["alpha_vantage"]["API_KEY"], output_format="pandas")
-    indicators = TechIndicators(key=config["alpha_vantage"]["API_KEY"], output_format="pandas")
+    # alfavantage = TimeSeries(key=config["alpha_vantage"]["API_KEY"], output_format="pandas")
+    # indicators = TechIndicators(key=config["alpha_vantage"]["API_KEY"], output_format="pandas")
 
-    tradedata = dict()
-    tradedata["symbol"] = symbol[0]
-    # Get current ticker price
-    tradedata["close"] = symbol[2][0]
-    # Get json object with the intraday data and another with  the call's metadata
-    intraday, i_meta = alfavantage.get_intraday(symbol[0], interval="1min")
-    tradedata["intraday"] = intraday['4. close']
-    # Get the MACD
-    macd, macd_meta = indicators.get_macd(symbol[0], interval="1min")
-    tradedata["macd"] = macd["MACD"]
-    # Get the RSI
-    rsi, rsi_meta = indicators.get_rsi(symbol[0], interval="1min", time_period=100)
-    tradedata["rsi"] = rsi["RSI"]
-    # Get the RoC
-    roc, roc_meta = indicators.get_roc(symbol[0], interval="1min", time_period=100)
-    tradedata["roc"] = roc["ROC"]
-    # Get stochastic oscillator
-    stoc, stoc_meta = indicators.get_stoch(symbol[0], interval="1min")
-    tradedata["stoc"] = stoc
-    # And finally, calculate the limit
-    limit_price = symbol[2][0] - symbol[2][0] * .1
-    tradedata["limit"] = limit_price
+    # tradedata = dict()
+    # tradedata["symbol"] = symbol[0]
+    # # Get current ticker price
+    # tradedata["close"] = symbol[2][0]
+    # # Get json object with the intraday data and another with  the call's metadata
+    # intraday, i_meta = alfavantage.get_intraday(symbol[0], interval="1min")
+    # tradedata["intraday"] = intraday['4. close']
+    # # Get the MACD
+    # macd, macd_meta = indicators.get_macd(symbol[0], interval="1min")
+    # tradedata["macd"] = macd["MACD"]
+    # # Get the RSI
+    # rsi, rsi_meta = indicators.get_rsi(symbol[0], interval="1min", time_period=100)
+    # tradedata["rsi"] = rsi["RSI"]
+    # # Get the RoC
+    # roc, roc_meta = indicators.get_roc(symbol[0], interval="1min", time_period=100)
+    # tradedata["roc"] = roc["ROC"]
+    # # Get stochastic oscillator
+    # stoc, stoc_meta = indicators.get_stoch(symbol[0], interval="1min")
+    # tradedata["stoc"] = stoc
+    # # And finally, calculate the limit
+    # limit_price = symbol[2][0] - symbol[2][0] * .1
+    # tradedata["limit"] = limit_price
 
-    return tradedata
+    return symbol
 
 
 if __name__ == "__main__":
