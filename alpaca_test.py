f07fa45690239f17912aab5c494447f3d5712913
Ben <beale.ben@gmail.com>
2019-09-17 08:15:33 -0400

    Grabbing some indicators and making some basic checks to see if we should enter a short position.

--- alpaca_test.py
+++ alpaca_test.py
@@ -1,4 +1,5 @@
 from statistics import mean
+from datetime import date, timedelta
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.sectorperformance import SectorPerformances
 from alpha_vantage.techindicators import TechIndicators
@@ -6,26 +7,28 @@ import alpaca_trade_api as tradeapi
 import configparser
 import pprint
 import sys
+import time
 import os
+
+config = configparser.ConfigParser()
+
+try:
+    config.read(os.path.relpath("config.ini"))
+except FileExistsError as e:
+    print("FileExistsError: {}".format(e))
+    sys.exit(1)
+
+pp = pprint.PrettyPrinter()
+
+api = tradeapi.REST(
+    base_url    = config["alpaca"]["APCA_API_BASE_URL"],
+    key_id      = config["alpaca"]["APCA_API_KEY_ID"],
+    secret_key  = config["alpaca"]["APCA_API_SECRET_KEY"],
+    api_version = config["alpaca"]["VERSION"]
+)
 
 
 def get_stuff_to_trade():
-    config = configparser.ConfigParser()
-
-    try:
-        config.read(os.path.relpath("config.ini"))
-    except FileExistsError as e:
-        print("FileExistsError: {}".format(e))
-        sys.exit(1)
-
-    pp = pprint.PrettyPrinter()
-
-    api                     = tradeapi.REST(
-        base_url    = config["alpaca"]["APCA_API_BASE_URL"],
-        key_id      = config["alpaca"]["APCA_API_KEY_ID"],
-        secret_key  = config["alpaca"]["APCA_API_SECRET_KEY"],
-        api_version = config["alpaca"]["VERSION"]
-    )
 
     account                 = api.get_account()
     print("Account #:       {}".format(account.account_number))
@@ -48,61 +51,61 @@ def get_stuff_to_trade():
     # Filter the assets down to just those on NASDAQ.
     nasdaq_assets = [a for a in active_assets if a.exchange == "NASDAQ"]
     vol_assets = []
-    for i in list(filter(lambda ass: ass.tradable == True, nasdaq_assets)):
-        symbol = i.symbol
+    for i in list(filter(lambda ass: ass.tradable is True, nasdaq_assets)):
 
-        barset = api.get_barset(symbol, "15Min", start="2019-09-01T09:30:00-04:00", end="2019-09-12T09:30:00-04:00")
+        symbol = i.symbol
+        today = date.fromtimestamp(time.time()).strftime("%Y-%m-%dT09:30:00-04:00")
+        start = date.fromtimestamp(time.time() - 604800).strftime("%Y-%m-%dT09:30:00-04:00")
+        barset = api.get_barset(symbol, "1Min", start=start, end=today)
         symbol_bars = barset[symbol]
 
-        # get trading volume
+        # Get trading volume
         volume = [bar.v for bar in symbol_bars if bar is not None]
+
+        # And closing price
+        closeprices = [bar.c for bar in symbol_bars if bar is not None]
         if volume is None:
             continue
 
         else:
             if len(volume) > 0:
                 vmean = mean(volume)
-                vol_assets.append((symbol, vmean))
+                vol_assets.append((symbol, vmean, closeprices))
 
         if len(vol_assets) > 3:
             break
 
-        # Submit a market order to buy 1 share of Apple at market price
-        # api.submit_order(
-        #     symbol="AAPL",
-        #     qty=1,
-        #     side="buy",
-        #     type="market",
-        #     time_in_force="gtc"
-        # )
-
-        # Submit a limit order to attempt to sell 1 share of AMD at a
-        # particular price ($20.50) when the market opens
-        # api.submit_order(
-        #     symbol="AMD",
-        #     qty=1,
-        #     side="sell",
-        #     type="limit",
-        #     time_in_force="opg",
-        #     limit_price=20.50
-        # )
-
     symbol = sorted(vol_assets, key=lambda ass: ass[1], reverse=True)[0]
     print("Symbol: ", symbol)
-    asset = api.get_asset(symbol[0])
-    barset = api.get_barset(symbol[0], "15Min", after="2019-09-12T07:00:00-04:00")
-    symbol_bars = barset[symbol[0]]
+    # barset = api.get_barset(symbol[0], "15Min", after="2019-09-12T07:00:00-04:00")
+    # asset = api.get_asset(symbol[0])
+    # symbol_bars = barset[symbol[0]]
 
     alfavantage = TimeSeries(key=config["alpha_vantage"]["API_KEY"], output_format="pandas")
     indicators = TechIndicators(key=config["alpha_vantage"]["API_KEY"], output_format="pandas")
 
-    tradedata = {}
+    tradedata = dict()
+    tradedata["symbol"] = symbol[0]
+    # Get current ticker price
+    tradedata["close"] = symbol[2][0]
     # Get json object with the intraday data and another with  the call's metadata
-    intraday, i_meta_data = alfavantage.get_intraday(symbol[0], interval="60min")
-    tradedata["intraday"] = intraday
-
-    macd, macd_meta_data = indicators.get_macd(symbol[0], interval="daily")
-    tradedata["macd"] = macd
+    intraday, i_meta = alfavantage.get_intraday(symbol[0], interval="1min")
+    tradedata["intraday"] = intraday['4. close']
+    # Get the MACD
+    macd, macd_meta = indicators.get_macd(symbol[0], interval="1min")
+    tradedata["macd"] = macd["MACD"]
+    # Get the RSI
+    rsi, rsi_meta = indicators.get_rsi(symbol[0], interval="1min", time_period=100)
+    tradedata["rsi"] = rsi["RSI"]
+    # Get the RoC
+    roc, roc_meta = indicators.get_roc(symbol[0], interval="1min", time_period=100)
+    tradedata["roc"] = roc["ROC"]
+    # Get stochastic oscillator
+    stoc, stoc_meta = indicators.get_stoch(symbol[0], interval="1min")
+    tradedata["stoc"] = stoc
+    # And finally, calculate the limit
+    limit_price = symbol[2][0] - symbol[2][0] * .1
+    tradedata["limit"] = limit_price
 
     return tradedata
 
