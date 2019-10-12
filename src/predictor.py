8ac4f8818e3d1eaabebcee953122a12fe6921d00
Ben <beale.ben@gmail.com>
2019-10-12 15:08:07 -0400

    Started work on moving prediction stuff to Predictor class.

---
+++ src/predictor.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from alpaca_functions import get_returns, sort_returns
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import configparser
+
+
+class Predictor:
+
+    def __init__(self, dataframe):
+
+        self.sess       = tf.InteractiveSession()
+        self.dataframe  = dataframe
+
+    def get_securities_predictions(self):
+        """Loop through tickers and run prediction algorithm."""
+        for ticker in self.dataframe.keys():
+
+            # TODO: Need to iterate through each series in each data frame. Too lazy to do it now
+            self.dataframe[ticker] = (self.get_predictions(self.dataframe[ticker]))
+
+    def get_predictions(self, data):
+        """Given a dataframe, return training and test data
+
+        :param data:
+        :return:
+        """
+        data = data.astype(float)
+        size = 50
+        returns = get_returns(data)
+
+        # pass one of the above into sort_returns
+        ins, outs = sort_returns(returns, size)
+
+        div = int(.8 * ins.shape[0])
+        train_ins, train_outs = ins[:div], outs[:div]
+        test_ins, test_outs = ins[div:], outs[div:]
+
+        # sess = tf.InteractiveSession()
+        x = tf.placeholder(tf.float32, [None, size])
+        y_ = tf.placeholder(tf.float32, [None, 1])
+
+        # we define trainable variables for our model
+        W = tf.Variable(tf.random_normal([size, 1]))
+        b = tf.Variable(tf.random_normal([1]))
+
+        # we define our model: y = W*x + b
+        y = tf.matmul(x, W) + b
+
+        # MSE:
+        cost = tf.reduce_sum(tf.pow(y - y_, 2)) / (2 * 1000)
+        optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(cost)
+
+        # initialize variables to random values
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
+        # run optimizer on entire training data set many times
+        for epoch in range(20000):
+            self.sess.run(optimizer, feed_dict={x: train_ins, y_: train_outs.reshape(1, -1).T})
+            # every 1000 iterations record progress
+            if (epoch + 1) % 1000 == 0:
+                c = self.sess.run(cost, feed_dict={x: train_ins, y_: train_outs.reshape(1, -1).T})
+                print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(c))
+
+        # train
+        predict = y
+        p = self.sess.run(predict, feed_dict={x: train_ins})
+        position = 2 * ((p > 0) - .5)
+        train_returns = position.reshape(-1) * train_outs
+        # plot(np.cumprod(returns + 1))
+
+        # test
+        predict = y
+        p = self.sess.run(predict, feed_dict={x: test_ins})
+        position = 2 * ((p > 0) - .5)
+        test_returns = position.reshape(-1) * test_outs
+
+        return train_returns, test_returns
+
