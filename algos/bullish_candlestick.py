c94b0ace6208103844686d7c3966aad18fa4b611
Ben <beale.ben@gmail.com>
2019-11-23 14:28:13 -0500

    Removed buy signals from algo since they are now in asset_selector.

--- algos/bullish_candlestick.py
+++ algos/bullish_candlestick.py
@@ -10,30 +10,16 @@ def run(alpaca_api):
     # initial trade state
     trading_symbol  = None
     trading         = False
-    assets          = AssetSelector(alpaca_api, edgar_token=None).bullish_candlesticks(64, 20)
+    assets          = AssetSelector(alpaca_api, edgar_token=None).bullish_candlesticks(64, 100)
     indicators      = Indicators(alpaca_api, assets).get_cluster()
 
     # trade decision here
     for i in indicators.keys():
 
         instrument = indicators[i]
-        # check the most recent moving average convergence-divergence
-        # has the MACD crossed over its signal and is the MACD percentage change is positive?
-        if instrument["macd"].iloc[-1] > instrument["signal"].iloc[-1] and instrument["macd_ptc"].iloc[-1] > 0:
-            # if the MACD checks out, check the directional movement indicator
-            # has the DMI positive value crossed over the negative value? Is percentage change positive?
-            if instrument["DMp"].iloc[-1] > instrument["DMm"].iloc[-1] and bullish_sequence(instrument["DMm"].iloc[-3], instrument["DMm"].iloc[-2], instrument["DMm"].iloc[-1]):
-                # if DMI checks out, check the volume zone oscillator
-                # the VZO should cross into the bullish range and continue going up
-                if (instrument["vzo"].iloc[-3] < instrument["vzo"].iloc[-2] < -40 < instrument["vzo"].iloc[-1]) and \
-                        instrument["vzo_ptc"].iloc[-1] > 0:
-                    trading = True
-                    trading_symbol = str(i)
 
-                elif (instrument["vzo"].iloc[-2] < -40 < instrument["vzo"].iloc[-1]) and \
-                        instrument["vzo_ptc"].iloc[-1] > 0:
-                    trading = True
-                    trading_symbol = str(i)
+        # do the processing for machine learning and see which one (if any) to trade
+        # TODO: implement machine learning
 
         if trading_symbol is not None:
             break
