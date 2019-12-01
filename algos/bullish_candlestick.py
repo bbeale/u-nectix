f373cfc10fc538707f1656d28e924c153c88805d
Ben <beale.ben@gmail.com>
2019-12-01 02:33:49 -0500

    Passing a time to get_all_asset_indicators.

--- algos/bullish_candlestick.py
+++ algos/bullish_candlestick.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 from src.asset_selector import AssetSelector
 from src.indicators import Indicators
-from util import submit_buy_order, bullish_sequence
-
+from util import submit_buy_order, time_formatter
+import time
 
 def run(alpaca_api):
 
     # initial trade state
     trading_symbol  = None
     trading         = False
-    assets          = AssetSelector(alpaca_api, edgar_token=None).bullish_candlesticks(64, 100)
-    indicators      = Indicators(alpaca_api, assets).get_cluster()
+    assets          = AssetSelector(alpaca_api, edgar_token=None).bullish_candlesticks(64, 1)
+    indicators      = Indicators(alpaca_api, assets).get_all_asset_indicators(backdate=time_formatter(time.time() - (604800 * 54)))
 
     # trade decision here
     for i in indicators.keys():
