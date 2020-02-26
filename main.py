615edcf2f547110caccc9cf5db8a8677498f1260
Ben <beale.ben@gmail.com>
2020-02-26 09:15:57 -0500

    Added interface with Kraken cryptocurrency exchange.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from src.broker import Broker, BrokerException, BrokerValidationException
+from algos import bullish_overnight_hold, bullish_overnight_crypto_hold
+from src.krak_dealer import KrakDealer, KrakDealerException
+from pykrakenapi.pykrakenapi import KrakenAPI, KrakenAPIError
 from alpaca_trade_api.rest import REST, APIError
-from algos import bullish_overnight_hold
+import krakenex
 import configparser
 import argparse
 import sys
@@ -12,9 +15,13 @@ import os
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--backtest",
-        type=str,
         required=False,
+        action="store_true",
         help="run in backtest mode if true, otherwise run in live mode")
+    parser.add_argument("-c", "--crypto",
+        required=False,
+        action="store_true",
+        help="if true, trade cryptocurrency instead of stocks using the Kraken exchange.")
     parser.add_argument("-d", "--days",
         type=int,
         required=False,
@@ -54,45 +61,72 @@ def main(config, args):
         print("[!] FileExistsError: {}".format(e))
         sys.exit(1)
 
-    try:
-        alpaca = REST(
-            base_url    = config["alpaca"]["APCA_API_BASE_URL"],
-            key_id      = config["alpaca"]["APCA_API_KEY_ID"],
-            secret_key  = config["alpaca"]["APCA_API_SECRET_KEY"],
-            api_version = config["alpaca"]["VERSION"]
-        )
-    except APIError as error:
-        raise error
+    # are we trading crypto?
+    if args.crypto:
+        print("[-] do stuff with Kraken")
+        try:
+            api = krakenex.API()
+            k = KrakenAPI(api)
+        except KrakenAPIError as error:
+            raise error
 
-    # edgar_token = config["edgar"]["TOKEN"]
+        try:
+            broker = KrakDealer(k)
+        except KrakDealerException as error:
+            raise error
+        else:
+            # is our account restricted from trading?
+            if broker.trading_blocked:
+                raise KrakDealerException("[!] Insufficient balances across coins or account is currently restricted from trading.")
 
-    try:
-        broker = Broker(alpaca)
-    except (BrokerException, BrokerValidationException, Exception) as error:
-        raise error
+        # how much money can we use to open new positions?
+        print("[?] ${} is available in cash.".format(broker.trade_balance["tb"]))
+
+        """Run the algorithm."""
+        if args.mode is None:
+            args.mode = 'long'
+        if args.period is None:
+            args.period = "1D"
+        if args.days is None:
+            args.days = 30
+
+        bullish_overnight_crypto_hold.run(broker, args)
+
     else:
-        # is our account restricted from trading?
-        if broker.trading_blocked:
-            raise BrokerException("[!] Account is currently restricted from trading.")
+        # we must be trading stocks
+        try:
+            alpaca = REST(base_url=config["alpaca"]["APCA_API_BASE_URL"], key_id=config["alpaca"]["APCA_API_KEY_ID"],
+                secret_key=config["alpaca"]["APCA_API_SECRET_KEY"], api_version=config["alpaca"]["VERSION"])
+        except APIError as error:
+            raise error
 
-    # how much money can we use to open new positions?
-    print("[?] ${} is available in cash.".format(broker.cash))
+        try:
+            broker = Broker(alpaca)
+        except (BrokerException, BrokerValidationException, Exception) as error:
+            raise error
+        else:
+            # is our account restricted from trading?
+            if broker.trading_blocked:
+                raise BrokerException("[!] Account is currently restricted from trading.")
 
-    """Run the algorithm."""
-    if args.mode is None:
-        args.mode = 'long'
-    if args.period is None:
-        args.period = "1D"
-    if args.selection_method is None:
-        args.selection_method = "bullish_candlestick"
-    if args.days is None:
-        args.days = 30
-    if args.max is None:
-        args.max = 26
-    if args.min is None:
-        args.min = 6
+        # how much money can we use to open new positions?
+        print("[?] ${} is available in cash.".format(broker.cash))
 
-    bullish_overnight_hold.run(broker, args)
+        """Run the algorithm."""
+        if args.mode is None:
+            args.mode = 'long'
+        if args.period is None:
+            args.period = "1D"
+        if args.selection_method is None:
+            args.selection_method = "bullish_candlestick"
+        if args.days is None:
+            args.days = 30
+        if args.max is None:
+            args.max = 26
+        if args.min is None:
+            args.min = 6
+
+        bullish_overnight_hold.run(broker, args)
 
 
 if __name__ == "__main__":
---
+++ src/krak_dealer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pykrakenapi.pykrakenapi import KrakenAPIError
+from marshmallow import Schema, fields, ValidationError
+import pandas as pd
+import time
+
+
+class KrakDealer:
+
+    def __init__(self, api):
+        self.api = api
+        self.trading_account = self.get_account()
+        self.trade_balance = self.get_trade_balance()
+        self.trading_blocked = True if self.trade_balance["tb"] == float(0) else False
+
+    def get_account(self):
+        return self.api.get_account_balance()
+
+    def get_trade_balance(self, asset="XBT"):
+        return self.api.get_trade_balance(asset=asset)
+
+class KrakDealerException(Exception):
+    pass
+
+
+class KrakDealerSchema(Schema):
+    pass
