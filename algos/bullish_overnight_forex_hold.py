33b31d5b7ef36b1c9cfea17bcb28be3e29d35a99
Ben <beale.ben@gmail.com>
2020-03-01 19:01:17 -0500

    Forex algo placeholder.

---
+++ algos/bullish_overnight_forex_hold.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from util import calculate_tolerable_risk, calculate_position_size
+from datetime import datetime, timedelta
+from pytz import timezone
+import pandas as pd
+import statistics
+
+
+def run(broker, args):
+    pass
