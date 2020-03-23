7092e7531bf23a9039ccec096d234e07c13080d2
Ben <beale.ben@gmail.com>
2020-03-22 23:08:51 -0400

    Added KrakDealer methods + unit test placeholders that interact with the Kraken API.

--- src/krak_dealer.py
+++ src/krak_dealer.py
@@ -6,22 +6,134 @@ import pandas as pd
 import time
 
 
-"""
-    https://coinmarketcal.com/en/api#pricing
-"""
-
-
 class KrakDealer:
 
-    def __init__(self, api, pair):
+    def __init__(self, api):
         self.api = api
-        self.pair = pair
-        self.trade_balance = self.get_trade_balance()
         self.trading_account = self.get_account()
-        self.trading_blocked = True if self.trade_balance[self.pair]["tb"] == float(0) else False
+        self.pairs = [p for p in self.trading_account.axes]
+
+    def get_server_time(self):
+        """Get the current time from the Kraken server.
+
+        :return:
+        """
+        try:
+            result = self.api.get_server_time()
+        except KrakenAPIError as error:
+            raise error
+        else:
+            return result
 
     def get_account(self):
-        return self.api.get_account_balance()
+        """Get our trading account.
 
-    def get_trade_balance(self, asset="XBT"):
-        return self.api.get_trade_balance(asset=asset)
+        :return:
+        """
+        try:
+            result = self.api.get_account_balance()
+        except KrakenAPIError as error:
+            raise error
+        else:
+            return result
+
+    def get_trade_balance(self, asset):
+        """Get the available trading balance of an asset.
+
+        :param asset:
+        :return:
+        """
+        if not asset or asset is None:
+            raise BrokerValidationException("[!] Invalid asset.")
+
+        try:
+            result = self.api.get_trade_balance(asset=asset)
+        except KrakenAPIError as error:
+            raise error
+        else:
+            return result
+
+    def get_tradable_asset_pairs(self, info=None, pair=None):
+        """Get a list of tradeable asset pairs.
+
+        :param info:
+        :param pair:
+        :return:
+        """
+        try:
+            result = self.api.get_tradable_asset_pairs(info=info, pair=pair)
+        except KrakenAPIError as error:
+            raise error
+        else:
+            return result
+
+    def get_ticker_information(self, pair):
+        """Get recent ticker info for a given asset pair.
+
+        :param pair:
+        :return:
+        """
+        if not pair or pair is None:
+            raise BrokerValidationException("[!] Invalid asset pair.")
+
+        try:
+            result = self.api.get_ticker_information(pair=pair)
+        except KrakenAPIError as error:
+            raise error
+        else:
+            return result
+
+    def get_ohlc_data(self, pair, interval=1, since=None, ascending=False):
+        """Get an OHLC formatted data frame for a given asset pair.
+
+        :param pair:
+        :param interval:
+        :param since:
+        :param ascending:
+        :return:
+        """
+        if not pair or pair is None:
+            raise BrokerValidationException("[!] Invalid asset pair.")
+
+        try:
+            result = self.api.get_ohlc_data(pair, interval=interval, since=since, ascending=ascending)
+        except KrakenAPIError as error:
+            raise error
+        else:
+            return result
+
+    def get_order_book(self, pair, count=100, ascending=False):
+        """Get the order book for a given asset pair.
+
+        :param pair:
+        :param count:
+        :param ascending:
+        :return:
+        """
+        if not pair or pair is None:
+            raise BrokerValidationException("[!] Invalid asset pair.")
+
+        try:
+            result = self.api.get_order_book(pair, count, ascending)
+        except KrakenAPIError as error:
+            raise error
+        else:
+            return result
+
+    def get_recent_spread_data(self, pair, since=None, ascending=False):
+        """Get recent spread data for a given asset pair.
+
+        :param pair:
+        :param since:
+        :param ascending:
+        :return:
+        """
+        if not pair or pair is None:
+            raise BrokerValidationException("[!] Invalid asset pair.")
+
+        try:
+            result = self.api.get_recent_spread_data(pair, since, ascending)
+        except KrakenAPIError as error:
+            raise error
+        else:
+            return result
--- tests/src/test_krak_dealer.py
+++ tests/src/test_krak_dealer.py
@@ -12,8 +12,38 @@ class TestKrakDealer(TestCase):
     config = parse_configs("../../config.ini")
     api = krakenex.API(key=config["kraken"]["api_key"], secret=config["kraken"]["private_key"])
     kraken = KrakenAPI(api, tier="Starter")
-    krak_deaker = KrakDealer(kraken, "XRPUSD")
+    krak_deaker = KrakDealer(kraken)
+    asset = "XXRP"
+    pair = "XXRPXXBT"
+
+    def test_get_server_time(self):
+        servertime = self.krak_deaker.get_server_time()
+        print(servertime)
 
     def test_get_account(self):
         account = self.krak_deaker.get_account()
         print(account)
+
+    def test_get_trade_balance(self):
+        bal = self.krak_deaker.get_trade_balance(asset=self.asset)
+        print(bal)
+
+    def test_get_tradable_asset_pairs(self):
+        pairs = self.krak_deaker.get_tradable_asset_pairs()
+        print(pairs)
+
+    def test_get_ticker_information(self):
+        info = self.krak_deaker.get_ticker_information(pair=self.pair)
+        print(info)
+
+    def test_get_ohlc_data(self):
+        ohlc = self.krak_deaker.get_ohlc_data(pair=self.pair)
+        print(ohlc)
+
+    def test_get_order_book(self):
+        orderbook = self.krak_deaker.get_order_book(pair=self.pair)
+        print(orderbook)
+
+    def test_get_recent_spread_data(self):
+        spread = self.krak_deaker.get_recent_spread_data(pair=self.pair)
+        print(spread)
