596c84f2b608c477f35cd3bd4ea722c4c641a124
Ben <beale.ben@gmail.com>
2020-05-30 17:42:41 -0400

    Fixed some imports from the Ubuntu update/Python 3.8 default install on my local box. Also made forex and crypto exchange interface constructors consistent and added a method to AssetSelector.

--- src/forex_broker.py
+++ src/forex_broker.py
@@ -8,15 +8,25 @@ import time
 
 class ForexBroker:
 
-    def __init__(self, api):
+    def __init__(self, api, pair):
 
         if not api or api is None:
-            raise BrokerValidationException("[!] API instancerequired.")
+            raise BrokerValidationException('[!] API instance required.')
+
+        if not pair or pair is None:
+            raise BrokerValidationException('[!] Trading instrument required.')
 
         self.api = api
+        self.pair = pair
+        self.trading_account = self.get_account()
+        self.trade_balance = self.get_trade_balance(asset=self.pair)
+        self.trading_blocked = self.is_trading_blocked()
 
     def get_account(self):
         raise NotImplementedError
 
-    def get_trading(self):
+    def get_trade_balance(self, asset):
         raise NotImplementedError
+
+    def is_trading_blocked(self):
+        return True if self.trade_balance <= 0 else False
