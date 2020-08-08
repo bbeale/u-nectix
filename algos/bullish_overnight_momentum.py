9305b57a4db0186b91d52079a38d9fcf60a25adf
Ben <beale.ben@gmail.com>
2020-08-08 14:20:09 -0400

    WIP: Bullish HODL/swing trading strategy.

--- algos/bullish_overnight_momentum.py
+++ algos/bullish_overnight_momentum.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from src.asset_selector import AssetSelector, AssetValidationException
+from broker.broker import Broker
+from argparse import Namespace
 from broker import BrokerException
 from util import time_from_datetime
 from algos import BaseAlgo
@@ -13,94 +15,8 @@ import time
 
 class Algorithm(AssetSelector, BaseAlgo):
 
-    def __init__(self, broker, cli_args):
+    def __init__(self, broker: Broker, cli_args: Namespace):
         super().__init__(broker=broker, cli_args=cli_args, edgar_token=None)
-
-    def bullish_overnight_momentum(self, backtest=False, bt_offset=None):
-        """Given a list of assets, evaluate which ones are bullish and return a sample of each.
-
-        These method names should correspond with files in the algos/ directory.
-
-        :param backtest:
-        :param bt_offset:
-        """
-        if not self.poolsize or self.poolsize is None or self.poolsize is 0:
-            raise AssetValidationException("[!] Invalid pool size.")
-
-        self.portfolio = []
-        if backtest:
-            if not bt_offset or bt_offset is None:
-                raise AssetValidationException("[!] Must specify a number of periods to offset if backtesting.")
-            limit = bt_offset
-        else:
-            limit = 1000
-
-        for ass in self.tradeable_assets:
-            """ The extraneous stuff that currently happens before the main part of evaluate_candlestick """
-            if backtest:
-                # df = self.broker.get_asset_df(ass.symbol, self.period, start=time_from_datetime(self.backtest_beginning), end=time_from_datetime(self.beginning))
-                start = time_from_datetime(self.backtest_beginning)
-                end = time_from_datetime(self.beginning)
-            else:
-                # df = self.broker.get_asset_df(ass.symbol, self.period, start=time_from_datetime(self.beginning), end=time_from_datetime(self.now))
-                start = time_from_datetime(self.beginning)
-                end = time_from_datetime(self.now)
-
-            # get the dataframe
-            df = self.broker.get_asset_df(ass.symbol, self.period, limit=limit, start=start, end=end)
-
-            # guard clauses to make sure we have enough data to work with
-            if df is None or df.empty:
-                continue
-
-            # is the most recent date in the data frame the end date?
-            df_end_date = str(df.iloc[-1].name).split(' ')[0]
-            has_end_date = df_end_date in end
-            if not has_end_date:
-                continue
-
-            # throw it away if the price is out of our min-max range
-            close = df["close"].iloc[-1]
-            if close > self.max_stock_price or close < self.min_stock_price:
-                continue
-
-            # throw it away if the candlestick pattern is not bullish
-            pattern = self.candle_pattern_direction(df)
-            if pattern in ["bear", None]:
-                continue
-
-            mfi_signal = self.signaler.mfi_signal.buy(df)
-            if not mfi_signal:
-                continue
-
-            # assume the current symbol pattern is bullish and add to the portfolio
-            self.portfolio.append(ass.symbol)
-            if len(self.portfolio) >= self.poolsize:
-                # exit the filter process
-                break
-
-    def portfolio_allocation(self, data, total_risk):
-        """Calculate portfolio allocation size given a ratings dataframe and a cash amount.
-
-        :param data:
-        :param total_risk:
-        :return:
-        """
-        total_rating = data["rating"].sum()
-        shares = {}
-        risk_amt = total_risk
-        for _, row in data.iterrows():
-            numshares = int(float(row["rating"]) / float(total_rating) * float(risk_amt) / float(row["price"]))
-            if numshares > 10:
-                multiplier = int(numshares / 10)
-                numshares = multiplier * 10
-            shares[row["symbol"]] = numshares
-
-            risk_amt -= numshares * row["price"]
-        # debug
-        # for k, v in shares.items():
-        #     print("[*] Ticker: {}, Shares: {}".format(k, v))
-        return shares
 
     def total_asset_value(self, positions, date):
         """ does what it says
@@ -125,6 +41,29 @@ class Algorithm(AssetSelector, BaseAlgo):
             positions[symbol] = {"shares": positions[symbol], "value": positions[symbol] * open, "change": change}
             total_value += positions[symbol]["value"]
         return positions, total_value,
+
+    def portfolio_allocation(self, data, total_risk):
+        """Calculate portfolio allocation size given a ratings dataframe and a cash amount.
+
+        :param data:
+        :param total_risk:
+        :return:
+        """
+        total_rating = data["rating"].sum()
+        shares = {}
+        risk_amt = total_risk
+        for _, row in data.iterrows():
+            numshares = int(float(row["rating"]) / float(total_rating) * float(risk_amt) / float(row["price"]))
+            if numshares > 10:
+                multiplier = int(numshares / 10)
+                numshares = multiplier * 10
+            shares[row["symbol"]] = numshares
+
+            risk_amt -= numshares * row["price"]
+        # debug
+        # for k, v in shares.items():
+        #     print("[*] Ticker: {}, Shares: {}".format(k, v))
+        return shares
 
     def get_ratings(self, algo_time=None, window_size=5):
         """Calculate trade decision based on standard deviation of past volumes.
@@ -194,7 +133,7 @@ class Algorithm(AssetSelector, BaseAlgo):
         return ratings
 
 
-def run(broker, args):
+def run(broker: Broker, args: Namespace):
 
     if not broker or broker is None:
         raise BrokerException("[!] A broker instance is required.")
@@ -220,8 +159,7 @@ def run(broker, args):
     starting_amount = cash
     risk_amount = broker.calculate_tolerable_risk(cash, risk_pct)
     algorithm = Algorithm(broker, args)
-
-    symbols = algorithm.portfolio
+    symbols = [asset.symbol for asset in algorithm.portfolio]
     print("[*] Trading assets: {}".format(",".join(symbols)))
 
     if args.backtest:
